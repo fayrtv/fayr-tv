1326e207f833ff0db7380fe9ab5bdbe4a8016814
tvotan <tin.votan@theblackswan.ai>
2020-12-10 11:29:31 +0100

    first commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const BASE_HREF = "";
 
 // API endpoint for retrieving the attendees list, joining the room, and ending the room
-export const CHIME_ROOM_API = "https://5arvyla1x5.execute-api.eu-west-1.amazonaws.com/Prod/";
+export const CHIME_ROOM_API = "https://t7b37v25pa.execute-api.eu-central-1.amazonaws.com/Prod/";
 
 // Chime-SDK allows up to 16 attendee videos
 export const CHIME_ROOM_MAX_ATTENDEE = 48;
@@ -12,10 +12,10 @@ export const CHIME_ROOM_MAX_ATTENDEE = 48;
 export const DEFAULT_VIDEO_STREAM = "https://testamplifyvode-dev-output-1pqi1hdc.s3-eu-west-1.amazonaws.com/stream/index.m3u8"
 
 // Default Chat websocket link
-export const CHAT_WEBSOCKET = "wss://nexywm0v59.execute-api.eu-west-1.amazonaws.com/Prod";
+export const CHAT_WEBSOCKET = "wss://qqyfe2oy3c.execute-api.eu-central-1.amazonaws.com/Prod";
 
 // Chime-SDK logging level: INFO, WARN, ERROR, DEBUG
-export const CHIME_LOG_LEVEL = 'WARN';
+export const CHIME_LOG_LEVEL = 'DEBUG';
 
 // Chime-Web UI debugging logging: true / false
-export const DEBUG = false;
+export const DEBUG = true;
