6ba5c8ab03b779f42f678a2200f1f39e63361706
tvotan <tin.votan@theblackswan.ai>
2020-12-23 13:21:54 +0100

    update stream

--- src/config.js
+++ src/config.js
@@ -7,7 +7,8 @@ export const CHIME_ROOM_API = "https://vl8fth0307.execute-api.eu-central-1.amazo
 export const CHIME_ROOM_MAX_ATTENDEE = 12;
 
 // Default video stream to play inside the video player
-export const DEFAULT_VIDEO_STREAM = "https://tagesschau-lh.akamaihd.net/i/tagesschau_3@66339/index_3776_av-p.m3u8?sd=10&rebase=on";
+// export const DEFAULT_VIDEO_STREAM = "https://daserstehdut-lh.akamaihd.net/i/daserstehd_ut_de@629193/index_2_av-p.m3u8";
+export const DEFAULT_VIDEO_STREAM = "https://mcdn.daserste.de/daserste/de/master.m3u8";
 
 // Default Chat websocket link
 export const CHAT_WEBSOCKET = "wss://fml0rdsl83.execute-api.eu-central-1.amazonaws.com/Prod";
