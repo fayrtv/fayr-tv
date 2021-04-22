00aabd59a3be8fecc2cc9567808906fe5b924d26
tvotan <tin.votan@theblackswan.ai>
2021-04-22 10:52:59 +0200

    update stream

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ export const CHIME_ROOM_MAX_ATTENDEE = 12;
 
 // Default video stream to play inside the video player
 // export const DEFAULT_VIDEO_STREAM = "https://mcdn.daserste.de/daserste/de/master.m3u8";
-export const DEFAULT_VIDEO_STREAM = "https://d1nucbbxid94su.cloudfront.net/out/v1/5128994176b14c248f5aafa8ecd2f65e/ff80eaa188e54de9912916e85c71e7a1/a552eac1138343db8f4324bfeee01b57/index.m3u8";
+export const DEFAULT_VIDEO_STREAM = "https://d1e4t3xfu0q4ky.cloudfront.net/fce11652-5078-4322-8e80-d04a71977b20/AppleHLS1/manunitedintermilan001.m3u8";
 
 // Default Chat websocket link
 export const CHAT_WEBSOCKET = "wss://fml0rdsl83.execute-api.eu-central-1.amazonaws.com/Prod";
