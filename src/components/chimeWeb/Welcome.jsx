b3a5a3bc473f4285fa82fb0b4f676544dd2600f0
tvotan <tin.votan@theblackswan.ai>
2021-05-07 18:03:19 +0200

    update text

--- src/components/chimeWeb/Welcome.jsx
+++ src/components/chimeWeb/Welcome.jsx
@@ -82,6 +82,7 @@ class Welcome extends Component {
               <img src="https://i.ibb.co/wpz7sFz/fayrtv-logo-light.png" alt="fayrtv-logo" border="0" height="55"></img>
               {/* <h1>FAYR TV</h1> */}
               <h2>  </h2>
+              <b></b>
               <h2>Erlebe Live- und Sportevents wie noch nie zuvor!</h2>
               <h3>Erstelle eine Watch Party oder trete einer bei und verbringe mit deinen Freunden eine geile Zeit!</h3>
             </div>
