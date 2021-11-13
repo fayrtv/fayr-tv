4b961d0dbd1034f64cd7549bd1caeedc57b8d2e1
JosXa <info@josxa.dev>
2021-11-13 03:49:28 +0100

    Fixed compile break

--- src/components/chimeWeb/Welcome.jsx
+++ src/components/chimeWeb/Welcome.jsx
@@ -4,7 +4,6 @@ import * as config from "../../config";
 import Error from "./Error";
 import QRCodeView from "./QRCodeView";
 
-
 class Welcome extends Component {
     state = {
         role: "host",
@@ -95,14 +94,12 @@ class Welcome extends Component {
                                 height="200"
                             />
                             {/* <h1>FAYR TV</h1> */}
-                            <h2> </h2>
-                            <b />
                             <img
                                 src="https://i.ibb.co/jGzKGYw/fayrtv-logo.png"
                                 alt="fayrtv-logo"
                                 border="0"
                                 height="200"
-                            ></img>
+                            />
                             <h2>Erlebe Live- und Sportevents wie noch nie zuvor!</h2>
                             <h3>
                                 Erstelle eine Watch Party oder trete einer bei und verbringe mit
@@ -161,53 +158,6 @@ class Welcome extends Component {
             </>
         );
     }
-
-    sessionStorage.setItem(`chime[${roomCode}]`, JSON.stringify(data));
-    this.props.history.push(this.roomUrlRelative);
-  }
-
-  render() {
-    const { username, roomCode, playbackURL, } = this.state;
-    const createRoomDisabled = !username || !roomCode || !playbackURL;
-    return (
-      <>
-        <div className="welcome form-grid">
-
-          <div className="welcome__intro">
-            <div className="intro__inner formatted-text">
-              <img src="https://i.ibb.co/jGzKGYw/fayrtv-logo.png" alt="fayrtv-logo" border="0" height="200"/>
-
-              <h2>Erlebe Live- und Sportevents wie noch nie zuvor!</h2>
-              <h3>Erstelle eine Watch Party oder trete einer bei und verbringe mit deinen Freunden eine geile Zeit!</h3>
-            </div>
-          </div>
-
-          <div className="welcome__content pd-4">
-            <div className="content__inner">
-              <h2 className="mg-b-2">Starte eine Watch Party</h2>
-              <h3>Fiebere zusammen mit deinen Freunden mit und schaue dir Live- und Sportevents online an!</h3>
-              <form action="">
-                <fieldset className="mg-b-2">
-                  <input className="mg-b-2" type="text" placeholder="Dein Name" value={username} ref={this.inputRef} onChange={this.handleNameChange} />
-                  <input type="text" placeholder="Code" value={roomCode} onChange={this.handleRoomChange} />
-                  {/* <input type="text" placeholder="Playback URL" value={playbackURL} onChange={this.handlePlaybackURLChange} /> */}
-                  {roomCode && <QRCodeView content={this.roomUrlAbsolute} width={150} height={150} padding={1} /> }
-                  <button className="mg-t-2 btn btn--primary" disabled={createRoomDisabled} onClick={this.handleCreateRoom} >Watch Party</button>
-                </fieldset>
-              </form>
-            </div>
-          </div>
-
-        </div>
-        {this.state.showError && (
-          <Error
-            closeError={this.closeError}
-            errorMsg={this.state.errorMsg}
-          />
-        )}
-      </>
-    )
-  }
 }
 
 export default withRouter(Welcome);
