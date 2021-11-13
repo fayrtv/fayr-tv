d7a26bbcac3635cfe67bd38111a5a7ea7621a612
tvotan <tin.votan@theblackswan.ai>
2021-11-13 03:32:37 +0100

    fix logo on welcome screen

--- src/components/chimeWeb/Welcome.jsx
+++ src/components/chimeWeb/Welcome.jsx
@@ -91,10 +91,7 @@ class Welcome extends Component {
           <div className="welcome__intro">
             <div className="intro__inner formatted-text">
               <img src="https://i.ibb.co/jGzKGYw/fayrtv-logo.png" alt="fayrtv-logo" border="0" height="200"/>
-              {/* <h1>FAYR TV</h1> */}
-              <h2>  </h2>
-              <b/>
-              <img src="https://i.ibb.co/jGzKGYw/fayrtv-logo.png" alt="fayrtv-logo" border="0" height="200"></img>
+
               <h2>Erlebe Live- und Sportevents wie noch nie zuvor!</h2>
               <h3>Erstelle eine Watch Party oder trete einer bei und verbringe mit deinen Freunden eine geile Zeit!</h3>
             </div>
