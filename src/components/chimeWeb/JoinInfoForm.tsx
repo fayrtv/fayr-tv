a4b78f593d5af28fa706702327e1dea051829ea8
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2021-11-17 21:42:42 +0100

    - Formatting, this time with prettier...

--- src/components/chimeWeb/JoinInfoForm.tsx
+++ src/components/chimeWeb/JoinInfoForm.tsx
@@ -2,74 +2,76 @@ import QRCodeView from "./QRCodeView";
 import { formatJoinRoomUrl } from "./Intro/urls";
 import React, { SetStateAction } from "react";
 import { useMediaQuery } from "react-responsive";
-import classNames from 'classnames';
+import classNames from "classnames";
 import { isFalsyOrWhitespace } from "util/stringUtils";
 
 import styles from "./JoinInfoForm.module.scss";
 
 type Props = {
-	username: string;
-	onUsernameChanged: React.Dispatch<SetStateAction<string>>;
-	roomCode: string;
-	onRoomCodeChanged: React.Dispatch<SetStateAction<string>>;
-	onSubmit: () => void;
-	disableSubmit?: boolean;
-	usernameInputRef?: React.Ref<HTMLInputElement>;
-	roomCodeInputRef?: React.Ref<HTMLInputElement>;
+    username: string;
+    onUsernameChanged: React.Dispatch<SetStateAction<string>>;
+    roomCode: string;
+    onRoomCodeChanged: React.Dispatch<SetStateAction<string>>;
+    onSubmit: () => void;
+    disableSubmit?: boolean;
+    usernameInputRef?: React.Ref<HTMLInputElement>;
+    roomCodeInputRef?: React.Ref<HTMLInputElement>;
 };
 
 export function JoinInfoForm({
-	username,
-	onUsernameChanged,
-	roomCode,
-	onRoomCodeChanged,
-	onSubmit,
-	usernameInputRef,
-	roomCodeInputRef,
-	disableSubmit = false,
+    username,
+    onUsernameChanged,
+    roomCode,
+    onRoomCodeChanged,
+    onSubmit,
+    usernameInputRef,
+    roomCodeInputRef,
+    disableSubmit = false,
 }: Props) {
-	const isValid = !isFalsyOrWhitespace(roomCode) && !isFalsyOrWhitespace(username);
-	const isMobile = useMediaQuery({ maxWidth: 960 });
+    const isValid = !isFalsyOrWhitespace(roomCode) && !isFalsyOrWhitespace(username);
+    const isMobile = useMediaQuery({ maxWidth: 960 });
 
-	const qrSize = isMobile ? 100 : 150;
+    const qrSize = isMobile ? 100 : 150;
 
-	return (
-		<form action="">
-			<fieldset className={classNames("mg-b-2", styles.JoinInfoForm, { [styles.Mobile]: isMobile })}>
-				<div>
-					<input
-						className="mg-b-2"
-						ref={usernameInputRef}
-						type="text"
-						placeholder="Dein Name"
-						value={username}
-						onChange={(ev) => onUsernameChanged(ev.target.value)}
-					/>
-					<input
-						ref={roomCodeInputRef}
-						type="text"
-						placeholder="Code"
-						value={roomCode}
-						onChange={(ev) => onRoomCodeChanged(ev.target.value)}
-					/>
-					<button
-						className="mg-t-2 btn btn--primary"
-						disabled={!isValid || disableSubmit}
-						onClick={onSubmit}
-					>
-						Watch Party
-					</button>
-				</div>
-				{/* <input type="text" placeholder="Playback URL" value={playbackURL} onChange={this.handlePlaybackURLChange} /> */}
-				{roomCode && (
-					<QRCodeView
-						content={formatJoinRoomUrl(roomCode)}
-						width={qrSize}
-						height={qrSize}
-						padding={1}
-					/>
-				)}
-			</fieldset>
-		</form>
-	);
+    return (
+        <form action="">
+            <fieldset
+                className={classNames("mg-b-2", styles.JoinInfoForm, { [styles.Mobile]: isMobile })}
+            >
+                <div>
+                    <input
+                        className="mg-b-2"
+                        ref={usernameInputRef}
+                        type="text"
+                        placeholder="Dein Name"
+                        value={username}
+                        onChange={(ev) => onUsernameChanged(ev.target.value)}
+                    />
+                    <input
+                        ref={roomCodeInputRef}
+                        type="text"
+                        placeholder="Code"
+                        value={roomCode}
+                        onChange={(ev) => onRoomCodeChanged(ev.target.value)}
+                    />
+                    <button
+                        className="mg-t-2 btn btn--primary"
+                        disabled={!isValid || disableSubmit}
+                        onClick={onSubmit}
+                    >
+                        Watch Party
+                    </button>
+                </div>
+                {/* <input type="text" placeholder="Playback URL" value={playbackURL} onChange={this.handlePlaybackURLChange} /> */}
+                {roomCode && (
+                    <QRCodeView
+                        content={formatJoinRoomUrl(roomCode)}
+                        width={qrSize}
+                        height={qrSize}
+                        padding={1}
+                    />
+                )}
+            </fieldset>
+        </form>
+    );
 }
--- src/components/chimeWeb/RemoteVideoGroup.tsx
+++ src/components/chimeWeb/RemoteVideoGroup.tsx
@@ -16,224 +16,224 @@ const MAX_REMOTE_VIDEOS = config.CHIME_ROOM_MAX_ATTENDEE;
 type Roster = Array<Attendee>;
 
 type Props = {
-	chime: IChimeSdkWrapper;
-	joinInfo: JoinInfo;
-	storedRoster: Roster;
+    chime: IChimeSdkWrapper;
+    joinInfo: JoinInfo;
+    storedRoster: Roster;
 };
 
 export const RemoteVideoGroup = ({ chime, joinInfo, storedRoster }: Props) => {
-	const [roster, setRoster] = React.useState<Roster>([]);
+    const [roster, setRoster] = React.useState<Roster>([]);
 
-	const previousRoster = React.useRef<Roster>([]);
+    const previousRoster = React.useRef<Roster>([]);
 
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	const findRosterSlot = (attendeeId: string) => {
-		for (let index = 0; index < roster.length; index++) {
-			if (roster[index].attendeeId === attendeeId || !roster[index].attendeeId) {
-				return index;
-			}
-		}
-		return 0;
-	};
+    const findRosterSlot = (attendeeId: string) => {
+        for (let index = 0; index < roster.length; index++) {
+            if (roster[index].attendeeId === attendeeId || !roster[index].attendeeId) {
+                return index;
+            }
+        }
+        return 0;
+    };
 
-	const videoTileDidUpdateCallback = (tileState: VideoTileState) => {
-		if (
-			!tileState.boundAttendeeId ||
-			tileState.localTile ||
-			tileState.isContent ||
-			!tileState.tileId
-		) {
-			return;
-		}
+    const videoTileDidUpdateCallback = (tileState: VideoTileState) => {
+        if (
+            !tileState.boundAttendeeId ||
+            tileState.localTile ||
+            tileState.isContent ||
+            !tileState.tileId
+        ) {
+            return;
+        }
 
-		let index = findRosterSlot(tileState.boundAttendeeId);
+        let index = findRosterSlot(tileState.boundAttendeeId);
 
-		const newRoster = [...roster];
+        const newRoster = [...roster];
 
-		const attendee =
-			newRoster[index] ??
-			previousRoster.current.find((x) => x.attendeeId === tileState.boundAttendeeId);
+        const attendee =
+            newRoster[index] ??
+            previousRoster.current.find((x) => x.attendeeId === tileState.boundAttendeeId);
 
-		newRoster[index] = {
-			...attendee,
-			videoEnabled: tileState.active,
-			attendeeId: tileState.boundAttendeeId,
-			tileId: tileState.tileId,
-		} as Attendee;
+        newRoster[index] = {
+            ...attendee,
+            videoEnabled: tileState.active,
+            attendeeId: tileState.boundAttendeeId,
+            tileId: tileState.tileId,
+        } as Attendee;
 
-		setRoster(newRoster);
+        setRoster(newRoster);
 
-		setTimeout(() => {
-			if (config.DEBUG) {
-				console.log(roster[index]);
-			}
+        setTimeout(() => {
+            if (config.DEBUG) {
+                console.log(roster[index]);
+            }
 
-			const videoElement = document.getElementById(`video_${tileState.boundAttendeeId}`);
+            const videoElement = document.getElementById(`video_${tileState.boundAttendeeId}`);
 
-			if (videoElement) {
-				chime.audioVideo.bindVideoElement(
-					tileState.tileId!,
-					videoElement as HTMLVideoElement,
-				);
-			}
-		}, 1000);
-	};
+            if (videoElement) {
+                chime.audioVideo.bindVideoElement(
+                    tileState.tileId!,
+                    videoElement as HTMLVideoElement,
+                );
+            }
+        }, 1000);
+    };
 
-	const videoTileWasRemovedCallback = (tileId: number) => {
-		// Find the removed tileId in the roster and mark the video as disabled.
-		// eslint-disable-next-line
-		const index = roster.findIndex((o) => o.tileId === tileId);
+    const videoTileWasRemovedCallback = (tileId: number) => {
+        // Find the removed tileId in the roster and mark the video as disabled.
+        // eslint-disable-next-line
+        const index = roster.findIndex((o) => o.tileId === tileId);
 
-		if (index === -1) {
-			return;
-		}
+        if (index === -1) {
+            return;
+        }
 
-		const newRoster = [...roster];
-		newRoster[index].videoEnabled = false;
-		setRoster(newRoster);
+        const newRoster = [...roster];
+        newRoster[index].videoEnabled = false;
+        setRoster(newRoster);
 
-		if (config.DEBUG) {
-			console.log(`Tile was removed ${tileId}`);
-		}
-	};
+        if (config.DEBUG) {
+            console.log(`Tile was removed ${tileId}`);
+        }
+    };
 
-	const rosterCallback = (newRoster: RosterMap) => {
-		if (Object.keys(newRoster).length > 2) {
-			if (config.DEBUG) {
-				console.log("More than 2");
-			}
-		}
+    const rosterCallback = (newRoster: RosterMap) => {
+        if (Object.keys(newRoster).length > 2) {
+            if (config.DEBUG) {
+                console.log("More than 2");
+            }
+        }
 
-		if (Object.keys(newRoster).length < previousRoster.current.length) {
-			if (config.DEBUG) {
-				console.log("Attendee(s) left");
-			}
+        if (Object.keys(newRoster).length < previousRoster.current.length) {
+            if (config.DEBUG) {
+                console.log("Attendee(s) left");
+            }
 
-			const differ = previousRoster.current.filter(
-				(_, k) => previousRoster.current[k] !== newRoster[k],
-			);
+            const differ = previousRoster.current.filter(
+                (_, k) => previousRoster.current[k] !== newRoster[k],
+            );
 
-			if (config.DEBUG) {
-				console.log(differ);
-			}
+            if (config.DEBUG) {
+                console.log(differ);
+            }
 
-			if (differ.length) {
-				for (let attendee of differ) {
-					const index = findRosterSlot(attendee.attendeeId);
+            if (differ.length) {
+                for (let attendee of differ) {
+                    const index = findRosterSlot(attendee.attendeeId);
 
-					const newRoster = [...roster];
-					newRoster[index] = {
-						...roster[index],
-						videoElement: roster[index].videoElement,
-					} as Attendee;
-					setRoster(newRoster);
-				}
-			}
-		}
+                    const newRoster = [...roster];
+                    newRoster[index] = {
+                        ...roster[index],
+                        videoElement: roster[index].videoElement,
+                    } as Attendee;
+                    setRoster(newRoster);
+                }
+            }
+        }
 
-		previousRoster.current = Object.entries(newRoster).map(([attendeeId, attendee]) => ({
-			...attendee,
-			attendeeId,
-		}));
+        previousRoster.current = Object.entries(newRoster).map(([attendeeId, attendee]) => ({
+            ...attendee,
+            attendeeId,
+        }));
 
-		dispatch(replaceRemoteVideoRoster(previousRoster.current));
+        dispatch(replaceRemoteVideoRoster(previousRoster.current));
 
-		let attendeeId;
-		for (attendeeId in newRoster) {
-			// Exclude self
-			if (attendeeId === joinInfo.Attendee.AttendeeId) {
-				continue;
-			}
+        let attendeeId;
+        for (attendeeId in newRoster) {
+            // Exclude self
+            if (attendeeId === joinInfo.Attendee.AttendeeId) {
+                continue;
+            }
 
-			// exclude empty name
-			if (!newRoster[attendeeId].name) {
-				continue;
-			}
+            // exclude empty name
+            if (!newRoster[attendeeId].name) {
+                continue;
+            }
 
-			const index = findRosterSlot(attendeeId);
+            const index = findRosterSlot(attendeeId);
 
-			const attendee = {
-				...roster[index],
-				...newRoster[attendeeId],
-			};
+            const attendee = {
+                ...roster[index],
+                ...newRoster[attendeeId],
+            };
 
-			attendee.attendeeId = attendeeId;
+            attendee.attendeeId = attendeeId;
 
-			roster[index] = attendee;
-			setRoster(roster);
-		}
-	};
+            roster[index] = attendee;
+            setRoster(roster);
+        }
+    };
 
-	React.useEffect(() => {
-		const roster: Roster = [];
-		previousRoster.current = storedRoster;
+    React.useEffect(() => {
+        const roster: Roster = [];
+        previousRoster.current = storedRoster;
 
-		for (let i = 0; i < MAX_REMOTE_VIDEOS; ++i) {
-			roster[i] = {
-				videoElement: React.createRef(),
-			} as Attendee;
-		}
+        for (let i = 0; i < MAX_REMOTE_VIDEOS; ++i) {
+            roster[i] = {
+                videoElement: React.createRef(),
+            } as Attendee;
+        }
 
-		setRoster(roster);
+        setRoster(roster);
 
-		chime.subscribeToRosterUpdate(rosterCallback);
+        chime.subscribeToRosterUpdate(rosterCallback);
 
-		if (chime.audioVideo) {
-			chime.audioVideo.addObserver({
-				videoTileDidUpdate: videoTileDidUpdateCallback,
-				videoTileWasRemoved: videoTileWasRemovedCallback,
-			});
-		}
+        if (chime.audioVideo) {
+            chime.audioVideo.addObserver({
+                videoTileDidUpdate: videoTileDidUpdateCallback,
+                videoTileWasRemoved: videoTileWasRemovedCallback,
+            });
+        }
 
-		return () => {
-			chime.unsubscribeFromRosterUpdate(rosterCallback);
-			try {
-				chime.audioVideo?.removeObserver({
-					videoTileDidUpdate: videoTileDidUpdateCallback,
-				});
-				chime.audioVideo?.removeObserver({
-					videoTileWasRemoved: videoTileWasRemovedCallback,
-				});
-			} catch (ex) {
-				// ok
-			}
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [chime]);
+        return () => {
+            chime.unsubscribeFromRosterUpdate(rosterCallback);
+            try {
+                chime.audioVideo?.removeObserver({
+                    videoTileDidUpdate: videoTileDidUpdateCallback,
+                });
+                chime.audioVideo?.removeObserver({
+                    videoTileWasRemoved: videoTileWasRemovedCallback,
+                });
+            } catch (ex) {
+                // ok
+            }
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [chime]);
 
-	return (
-		<Grid
-			gridProperties={{
-				gap: 0,
-				gridTemplateRows: "repeat(4, 1fr)",
-				gridTemplateColumns: "repeat(2, 50%)",
-			}}
-			className="RemoteVideoGroup"
-		>
-			{roster.slice(0, 10).map((attendee, index) => {
-				return (
-					<Cell key={index}>
-						<RemoteVideo
-							chime={chime}
-							tileIndex={index}
-							key={index}
-							attendeeId={attendee.attendeeId}
-							videoEnabled={attendee.videoEnabled}
-							name={attendee.name}
-							muted={attendee.muted}
-							volume={attendee.volume}
-							videoElement={attendee.videoElement}
-						/>
-					</Cell>
-				);
-			})}
-		</Grid>
-	);
+    return (
+        <Grid
+            gridProperties={{
+                gap: 0,
+                gridTemplateRows: "repeat(4, 1fr)",
+                gridTemplateColumns: "repeat(2, 50%)",
+            }}
+            className="RemoteVideoGroup"
+        >
+            {roster.slice(0, 10).map((attendee, index) => {
+                return (
+                    <Cell key={index}>
+                        <RemoteVideo
+                            chime={chime}
+                            tileIndex={index}
+                            key={index}
+                            attendeeId={attendee.attendeeId}
+                            videoEnabled={attendee.videoEnabled}
+                            name={attendee.name}
+                            muted={attendee.muted}
+                            volume={attendee.volume}
+                            videoElement={attendee.videoElement}
+                        />
+                    </Cell>
+                );
+            })}
+        </Grid>
+    );
 };
 
 const mapStateToProps = (store: ReduxStore): Pick<Props, "storedRoster"> => ({
-	storedRoster: store.remoteVideoReducer,
+    storedRoster: store.remoteVideoReducer,
 });
 
 export default connect(mapStateToProps)(RemoteVideoGroup);
