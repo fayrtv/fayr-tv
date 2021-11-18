d963390e57c9bdf10eb9066dca232929382de1cd
tvotan <tin.votan@theblackswan.ai>
2021-11-18 15:01:23 +0100

    redeployment on AWS - setting new API URL and WebSocket URI

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { makeid } from "util/guidHelper";
 export const BASE_HREF = "";
 
 // API endpoint for retrieving the attendees list, joining the room, and ending the room
-export const CHIME_ROOM_API = "https://vl8fth0307.execute-api.eu-central-1.amazonaws.com/Prod/";
+export const CHIME_ROOM_API = "https://enb885lh75.execute-api.eu-central-1.amazonaws.com/Prod/";
 // For local AWS SAM container:
 //export const CHIME_ROOM_API = "http://127.0.0.1:5859";
 
@@ -16,7 +16,7 @@ export const DEFAULT_VIDEO_STREAM =
 // export const DEFAULT_VIDEO_STREAM = "https://d3fouxdzjpljew.cloudfront.net/out/v1/11d6013fea984a00a36d75ec33e66cc1/12eb4ef2f7cf4f838a5119af953745de/d4183462b7db4490a631cc977efc0508/index.m3u8"; // football stream
 
 // Default Chat websocket link
-export const CHAT_WEBSOCKET = "wss://fml0rdsl83.execute-api.eu-central-1.amazonaws.com/Prod";
+export const CHAT_WEBSOCKET = "wss://glql810lxg.execute-api.eu-central-1.amazonaws.com/Prod	";
 
 // Chime-SDK logging level: INFO, WARN, ERROR, DEBUG
 export const CHIME_LOG_LEVEL = "DEBUG";
