6d4f71cb16f403546a013b95700f43a9c8f15413
JosXa <info@josxa.dev>
2021-11-27 18:51:11 +0100

    ServiceWorker to TypeScript

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -10,6 +10,8 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
+import { DEBUG } from "config";
+
 const isLocalhost = Boolean(
     window.location.hostname === "localhost" ||
         // [::1] is the IPv6 localhost address.
@@ -18,7 +20,7 @@ const isLocalhost = Boolean(
         window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 );
 
-export function register(config) {
+export function register(config: any) {
     if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
         // The URL constructor is available in all browsers that support SW.
         const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -52,7 +54,7 @@ export function register(config) {
     }
 }
 
-function registerValidSW(swUrl, config) {
+function registerValidSW(swUrl: string, config: any) {
     navigator.serviceWorker
         .register(swUrl)
         .then((registration) => {
@@ -96,7 +98,7 @@ function registerValidSW(swUrl, config) {
         });
 }
 
-function checkValidServiceWorker(swUrl, config) {
+function checkValidServiceWorker(swUrl: string, config: any) {
     // Check if the service worker can be found. If it can't reload the page.
     fetch(swUrl, {
         headers: { "Service-Worker": "script" },
@@ -128,7 +130,11 @@ export function unregister() {
     if ("serviceWorker" in navigator) {
         navigator.serviceWorker.ready
             .then((registration) => {
-                registration.unregister();
+                registration.unregister().then(() => {
+                    if (DEBUG) {
+                        console.debug("Service worker unregistered.");
+                    }
+                });
             })
             .catch((error) => {
                 console.error(error.message);
