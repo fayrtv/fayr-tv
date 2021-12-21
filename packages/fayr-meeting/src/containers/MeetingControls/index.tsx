e97ac5f390b30ba0d681814f789237a89936a00c
FAYR Engineering <73653296+fayrtv@users.noreply.github.com>
2021-12-21 23:53:30 +0100

    add FAYR Meeting

---
+++ packages/fayr-meeting/src/containers/MeetingControls/index.tsx
@@ -0,0 +1,61 @@
+// Copyright 2020-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import React from 'react';
+import {
+  ControlBar,
+  AudioInputVFControl,
+  AudioInputControl,
+  VideoInputBackgroundBlurControl,
+  ContentShareControl,
+  AudioOutputControl,
+  ControlBarButton,
+  useUserActivityState,
+  Dots,
+  VideoInputControl,
+} from 'amazon-chime-sdk-component-library-react';
+
+import EndMeetingControl from '../EndMeetingControl';
+import { useNavigation } from '../../providers/NavigationProvider';
+import { StyledControls } from './Styled';
+import { useAppState } from '../../providers/AppStateProvider';
+import { BlurValues } from '../../types';
+
+
+const MeetingControls: React.FC = () => {
+  const { toggleNavbar, closeRoster, showRoster } = useNavigation();
+  const { isUserActive } = useUserActivityState();
+  const { isWebAudioEnabled, blurOption } = useAppState();
+  const isBackgroundBlurEnabled = blurOption !== BlurValues.blurDisabled;
+
+  const handleToggle = (): void => {
+    if (showRoster) {
+      closeRoster();
+    }
+    toggleNavbar();
+  };
+
+  return (
+    <StyledControls className="controls" active={!!isUserActive}>
+      <ControlBar
+        className="controls-menu"
+        layout="undocked-horizontal"
+        showLabels
+      >
+        <ControlBarButton
+          className="mobile-toggle"
+          icon={<Dots />}
+          onClick={handleToggle}
+          label="Menu"
+        />
+        { isWebAudioEnabled ? <AudioInputVFControl /> :  <AudioInputControl /> }
+        { isBackgroundBlurEnabled ? <VideoInputBackgroundBlurControl/> : <VideoInputControl/> }
+        <ContentShareControl />
+        <AudioOutputControl />
+        <EndMeetingControl />
+      </ControlBar>
+    </StyledControls>
+  );
+};
+
+export default MeetingControls;
---
+++ packages/fayr-meeting/src/types/index.ts
@@ -0,0 +1,53 @@
+// Copyright 2020-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+export type FormattedDeviceType = {
+  deviceId: string;
+  label: string;
+};
+
+export type DeviceType = MediaDeviceInfo | FormattedDeviceType;
+
+export type SelectedDeviceType = string | null;
+
+export type DeviceTypeContext = {
+  devices: DeviceType[];
+  selectedDevice: SelectedDeviceType;
+};
+
+export type LocalVideoContextType = {
+  isVideoEnabled: boolean;
+  toggleVideo: () => Promise<void>;
+};
+
+export type DeviceConfig = {
+  additionalDevices?: boolean;
+};
+
+export type LocalAudioOutputContextType = {
+  isAudioOn: boolean;
+  toggleAudio: () => void;
+};
+
+export type ContentShareControlContextType = {
+  isContentSharePaused: boolean;
+  toggleContentShare: () => Promise<void>;
+  togglePauseContentShare: () => void;
+};
+
+export enum MeetingMode {
+  Spectator,
+  Attendee,
+}
+
+export enum Layout {
+  Gallery,
+  Featured,
+}
+
+export const BlurValues = {
+  blurDisabled: "0",
+  blur10Percent: "10",
+  blur20Percent: "20",
+  blur40Percent: "40",
+}
