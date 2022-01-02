49791376e3d66cb1217408d24c7a4f7d753663e3
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2022-01-02 20:29:11 +0100

    - Fixed build

--- packages/shared-components/src/index.ts
+++ packages/shared-components/src/index.ts
@@ -1,7 +1,7 @@
 import Flex from "./Flex";
-import { MaterialIcon } from "./MaterialIcon";
 import Cell from "./GridLayout/Cell";
 import Grid from "./GridLayout/Grid";
+import { MaterialIcon } from "./MaterialIcon";
 import LoadingAnimation from "./interactivity/LoadingAnimation";
 
 export { Flex, Cell, Grid, LoadingAnimation, MaterialIcon };
