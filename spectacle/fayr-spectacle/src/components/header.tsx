60ce6ed23c0fe39b8538194c8d35fc6550d7bdab
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2022-05-03 23:00:24 +0200

    - Removed footer.tsx - Created header

--- spectacle/fayr-spectacle/src/components/header.tsx
+++ spectacle/fayr-spectacle/src/components/header.tsx
@@ -1,31 +1,41 @@
-import { Box, Container, Text, Group } from "@mantine/core";
+import { Box, Container, Text, Group, Sx, Burger } from "@mantine/core";
 import Link from "next/link";
+import React from "react";
 
-import ThemeToggleButton from "../components/theme-toggle-button";
+const defaultTextProps: Sx = { fontSize: "1em", fontWeight: 600 };
 
-const Header = () => (
-    <Box
-        component="header"
-        py="md"
-        sx={{ textAlign: "center", borderBottom: "1px solid lightgray" }}
-    >
-        <Container>
-            <Group>
-                <Text
-                    variant="gradient"
-                    gradient={{ from: "primary", to: "secondary", deg: 45 }}
-                    sx={{ fontSize: "1.5em", fontWeight: 800 }}
-                >
-                    <Link href="/">ZEISS VISION CENTER Osnabrück</Link>
-                </Text>
+const Header = () => {
+    const [burgerOpen, setBurgerOpen] = React.useState(false);
 
-                {/* pushes the succeeding contents to the right */}
-                <span style={{ flexGrow: 1 }} />
-
-                <ThemeToggleButton />
-            </Group>
-        </Container>
-    </Box>
-);
+    return (
+        <Box
+            component="header"
+            py="md"
+            sx={{
+                textAlign: "center",
+                borderBottom: "1px solid lightgray",
+                backgroundColor: "#000000",
+            }}
+        >
+            <Container>
+                <Group spacing="xs">
+                    <Text color="white" sx={defaultTextProps}>
+                        <Link href="/">ZEISS VISION CENTER Osnabrück</Link>
+                    </Text>
+                    <Group direction="row" position="apart" style={{ width: "100%" }}>
+                        <Text color="#4498D8" sx={defaultTextProps}>
+                            Digitaler Brillenpass
+                        </Text>
+                        <Burger
+                            color="white"
+                            opened={burgerOpen}
+                            onClick={() => setBurgerOpen((curr) => !curr)}
+                        />
+                    </Group>
+                </Group>
+            </Container>
+        </Box>
+    );
+};
 
 export default Header;
