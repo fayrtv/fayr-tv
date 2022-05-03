7974ac83621ad5dc84b8dfa3ebd2b71317be44a0
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2022-05-04 00:08:54 +0200

    - Progress dump

--- spectacle/fayr-spectacle/src/pages/index.tsx
+++ spectacle/fayr-spectacle/src/pages/index.tsx
@@ -3,49 +3,13 @@ import { NextPageWithLayout } from "~/types/next-types";
 
 import MainLayout from "../components/MainLayout";
 import Layout from "../components/layout";
+import Link from "next/link";
 
 const Home: NextPageWithLayout = () => {
     return (
         <Layout>
             <>
-                <Title order={1} sx={{ textAlign: "center" }}>
-                    Welcome.
-                </Title>
-                <Text my="sm">This is a frontend starter repository that uses:</Text>
-                <List>
-                    <List.Item>
-                        <Anchor href="https://nextjs.org/">NextJS</Anchor>
-                    </List.Item>
-                    <List.Item>
-                        <Anchor href="https://www.typescriptlang.org/">TypeScript</Anchor>
-                    </List.Item>
-                    <List.Item>
-                        <Anchor href="https://mantine.dev/">MantineUI</Anchor>
-                    </List.Item>
-                    <List.Item>
-                        <Anchor href="https://tabler-icons-react.vercel.app/">
-                            Tabler React Icons
-                        </Anchor>
-                    </List.Item>
-                    <List.Item>
-                        <Anchor href="https://github.com/Web3Modal/web3modal">Web3Modal</Anchor>
-                    </List.Item>
-                    <List.Item>
-                        <Anchor href="https://docs.ethers.io/v5/">Ethers</Anchor>
-                    </List.Item>
-                </List>
-                <Text my="sm">
-                    You can connect via injected wallet (e.g.{" "}
-                    <Anchor href="https://metamask.io/">MetaMask</Anchor>) and this will enable
-                    access to your wallet via a custom context. You can then use this context to
-                    interact with Web3. For starters, click on the wallet icon to see your wallet
-                    details. To change the wallet connection logic, change
-                    `context/web3.context.tsx`.
-                </Text>
-                <Text my="sm">
-                    The template also has a premade layout component, and supports dark/light theme.
-                    To change theme, click on the sun icon on the top right.
-                </Text>
+                <Link href={"/content/spectaclepass"}>SpectaclePass</Link>
             </>
         </Layout>
     );
