9450140c6899f64271cca37c8f8b03f14726d826
Joscha Götzer <joscha.goetzer@gmail.com>
2022-05-21 12:44:17 +0200

    Refraction protocols are medical data

--- spectacle/fayr-spectacle/src/types/refraction-protocol.ts
+++ spectacle/fayr-spectacle/src/types/refraction-protocol.ts
@@ -1,12 +1,15 @@
+/**
+ * SENSITIVE, CLIENT-SIDE-ONLY DATA
+ */
+export type RefractionProtocol = {
+    left: SideConfiguration;
+    right: SideConfiguration;
+};
+
 type SideConfiguration = {
     sphere: number;
     cylinder: number;
     axis: number;
     addition?: number;
     pd: number;
-};
-
-export type RefractionProtocol = {
-    left: SideConfiguration;
-    right: SideConfiguration;
 };
