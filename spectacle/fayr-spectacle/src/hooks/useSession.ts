350525678efa8fcc633c1a2e16cb0e43de1fe47f
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2022-05-22 17:47:40 +0200

    - Fixed missing reference

--- spectacle/fayr-spectacle/src/hooks/useSession.ts
+++ spectacle/fayr-spectacle/src/hooks/useSession.ts
@@ -1,11 +1,11 @@
 import { Auth } from "aws-amplify";
-import { convertAmplifyAuthUser } from "~/helpers/authentication";
 import { useQuery } from "react-query";
+import { convertAwsModelToUser } from "~/helpers/awsModelParser";
 
 export const useSession = () => {
     const { isLoading, data: user } = useQuery("get-user", async () => {
         const userInfo = await Auth.currentUserInfo();
-        return userInfo?.attributes ? convertAmplifyAuthUser(userInfo) : undefined;
+        return userInfo?.attributes ? convertAwsModelToUser(userInfo) : undefined;
     });
     return { isLoading, user };
 };
