422549298048233d4ff06ed516a6073820c72585
Joscha Götzer <joscha.goetzer@gmail.com>
2022-05-23 01:23:29 +0200

    #214: Extracted SVG version of logo and created custom "seeing beyond" text below to make it work in dark mode

--- spectacle/fayr-spectacle/src/components/ZeissLogo.tsx
+++ spectacle/fayr-spectacle/src/components/ZeissLogo.tsx
@@ -1,4 +1,4 @@
-import { Image } from "@mantine/core";
+import { Box, Center, Image, Stack, Text, useMantineColorScheme } from "@mantine/core";
 import React from "react";
 
 type Props = Omit<React.ComponentProps<typeof Image>, "size" | "alt"> & {
@@ -6,5 +6,21 @@ type Props = Omit<React.ComponentProps<typeof Image>, "size" | "alt"> & {
 };
 
 export default function ZeissLogo({ size = 80, ...props }: Props) {
-    return <Image src={"/assets/zeiss_logo.svg"} alt="ZEISS Logo" width={size} {...props} />;
+    const { colorScheme } = useMantineColorScheme();
+    return (
+        <Stack align="center" spacing={14} style={{ userSelect: "none" }}>
+            <Image src={"/assets/zeiss-logo.svg"} alt="ZEISS Logo" width={size} {...props} />
+            <Text
+                color={colorScheme === "light" ? "black" : "white"}
+                sx={{
+                    fontSize: 14,
+                    fontFamily: "Frutiger Next",
+                    fontWeight: 500,
+                    letterSpacing: 0.4,
+                }}
+            >
+                Seeing beyond
+            </Text>
+        </Stack>
+    );
 }
