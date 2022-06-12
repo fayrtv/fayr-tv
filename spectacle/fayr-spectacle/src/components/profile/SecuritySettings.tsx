a362a740459e07116c14b3bcf6be26c185d1e890
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2022-06-12 22:17:43 +0200

    - Added import

--- spectacle/fayr-spectacle/src/components/profile/SecuritySettings.tsx
+++ spectacle/fayr-spectacle/src/components/profile/SecuritySettings.tsx
@@ -1,6 +1,25 @@
-import { Button, Divider, Group, Modal, Stack, Text, Textarea, ThemeIcon } from "@mantine/core";
+import {
+    Alert,
+    Button,
+    Divider,
+    Group,
+    Modal,
+    Popover,
+    Stack,
+    Text,
+    Textarea,
+    ThemeIcon,
+} from "@mantine/core";
 import * as React from "react";
-import { AlertTriangle, ArrowBarDown, ArrowBarUp, Check, Copy } from "tabler-icons-react";
+import {
+    AlertCircle,
+    AlertTriangle,
+    ArrowBarDown,
+    ArrowBarUp,
+    Check,
+    Copy,
+    FileImport,
+} from "tabler-icons-react";
 import { User } from "~/types/user";
 import useEncryption from "~/hooks/useEncryption";
 import ILocalEncryptionStorageHandler from "~/utils/encryption/localPersistence/ILocalEncryptionStorageHandler";
@@ -24,10 +43,110 @@ type ModalProps = {
     store: SerializedModel<Store>;
     user: User;
     localEncryptionManager: ILocalEncryptionStorageHandler;
+    closeModal(): void;
 };
 
-const ImportMenu = ({ user, store, localEncryptionManager }: ModalProps) => {
-    return <Group direction="row"></Group>;
+const ImportMenu = ({ user, store, localEncryptionManager, closeModal }: ModalProps) => {
+    enum View {
+        ConfirmRisk,
+        Import,
+    }
+
+    const [view, setView] = React.useState<View>(View.Import);
+    const [manuallyEnteredKey, setManuallyEnteredKey] = React.useState("");
+    const [importError, setImportError] = React.useState(false);
+
+    React.useEffect(() => {
+        const init = async () => {
+            const hasExistingKey = await localEncryptionManager.hasSecret(user.id, store.id);
+            if (hasExistingKey) {
+                setView(View.ConfirmRisk);
+            }
+        };
+        init();
+    }, []);
+
+    if (view === View.ConfirmRisk) {
+        return (
+            <Stack>
+                <Group direction="row" noWrap>
+                    <ThemeIcon color="yellow" size="xl">
+                        <AlertTriangle />
+                    </ThemeIcon>
+                    <span>
+                        Auf diesem Gerät ist bereits ein existierendes Verschlüsselungsgeheimnis
+                        vorhanden. Sollten sie ein neues Geheimnis importieren, um beispielsweise
+                        verschiedene Geräte zu synchronisieren, so wird das alte Geheimnis
+                        überschrieben. Importieren sie nur Daten von eigenen Geräten oder Personen
+                        welchen sie vertrauen.
+                    </span>
+                </Group>
+                <Button onClick={() => setView(View.Import)}>Weiter</Button>
+            </Stack>
+        );
+    }
+
+    return (
+        <Stack align="center">
+            <Group spacing="xs" grow direction="row" align="start">
+                <Stack sx={() => ({ flexGrow: 2 })} align="center">
+                    <Text underline>Per QR Code</Text>
+                    TODO
+                </Stack>
+                <Stack sx={() => ({ flexGrow: 2 })} align="center">
+                    <Text underline>Manuell</Text>
+                    <Text>
+                        Fügen sie hier den Schlüssel aus dem "Exportieren" Menü auf dem Quellgerät
+                        ein
+                    </Text>
+                    <Textarea
+                        autosize
+                        sx={(_) => ({ width: "100%" })}
+                        value={manuallyEnteredKey}
+                        onChange={(event) => setManuallyEnteredKey(event.currentTarget.value)}
+                    />
+                    {importError ? (
+                        <>
+                            <ThemeIcon color="red" size="xl">
+                                <AlertCircle />
+                            </ThemeIcon>
+                            <Alert icon={<AlertCircle size={16} />} title="Fehler!" color="red">
+                                Das hat leider nicht wie gewünscht funktioniert.
+                            </Alert>
+                        </>
+                    ) : (
+                        <Button
+                            leftIcon={<FileImport />}
+                            onClick={async () => {
+                                try {
+                                    const jsonKey = JSON.parse(window.atob(manuallyEnteredKey));
+                                    const exportedKey = await window.crypto.subtle.importKey(
+                                        "jwk",
+                                        jsonKey,
+                                        "AES-GCM",
+                                        true,
+                                        ["encrypt", "decrypt"],
+                                    );
+                                    await localEncryptionManager.setSecret(
+                                        exportedKey,
+                                        user.id,
+                                        store.id,
+                                    );
+                                    closeModal();
+                                } catch (error) {
+                                    setImportError(true);
+                                    window.setTimeout(() => setImportError(false), 1000);
+                                    console.log(error);
+                                }
+                            }}
+                        >
+                            Importieren
+                        </Button>
+                    )}
+                </Stack>
+            </Group>
+        </Stack>
+    );
 };
 
 const ExportMenu = ({ user, store, localEncryptionManager }: ModalProps) => {
@@ -130,12 +249,14 @@ export const SecuritySettings = ({ user }: Props) => {
                         user={user}
                         localEncryptionManager={localEncryptionManager}
                         store={storeInfo}
+                        closeModal={() => setModalOperation(undefined)}
                     />
                 ) : (
                     <ImportMenu
                         user={user}
                         localEncryptionManager={localEncryptionManager}
                         store={storeInfo}
+                        closeModal={() => setModalOperation(undefined)}
                     />
                 )}
             </Modal>
