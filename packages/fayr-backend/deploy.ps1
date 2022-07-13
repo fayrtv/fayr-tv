521e413afe23a82e54b068801be7f87fb78b9c34
Joscha Götzer <joscha.goetzer@gmail.com>
2022-07-14 00:18:41 +0200

    Made deploys easier

--- packages/fayr-backend/deploy.ps1
+++ packages/fayr-backend/deploy.ps1
@@ -5,11 +5,7 @@ param (
 
     [Parameter(Mandatory=$True, HelpMessage="The bucket to use for uploading")]
     [ValidateNotNullOrEmpty()]
-    [string]$bucket,
-
-    [Parameter(Mandatory=$True, HelpMessage="The name of this stack")]
-    [ValidateNotNullOrEmpty()]
-    [string]$stack
+    [string]$name
 )
 
 if (Test-Path -Path compiled)
@@ -23,10 +19,10 @@ $ErrorActionPreference = "Stop";
 Write-Output "Don't forget to run 'aws configure' with the 'eu-central-1' region first."
 
 Write-Output "Creating S3 bucket..."
-aws s3api create-bucket --bucket $bucket --region $region --create-bucket-configuration LocationConstraint=$region
+aws s3api create-bucket --bucket $name --region $region --create-bucket-configuration LocationConstraint=$region
 
 Write-Output "Packaging..."
-sam package --template-file template.yaml --output-template-file packaged.yaml --s3-bucket $bucket
+sam package --template-file template.yaml --output-template-file packaged.yaml --s3-bucket $name
 
 Write-Output "Deploying..."
-sam deploy --template-file .\packaged.yaml --stack-name $stack --capabilities CAPABILITY_IAM
+sam deploy --template-file .\packaged.yaml --stack-name $name --capabilities CAPABILITY_IAM
