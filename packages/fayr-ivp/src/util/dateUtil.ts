d0a97cb8d86d1207cfa4003d5478d0377be64038
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2022-07-15 23:06:00 +0200

    - Fixed mobile responsive start screens

--- packages/fayr-ivp/src/util/dateUtil.ts
+++ packages/fayr-ivp/src/util/dateUtil.ts
@@ -1,3 +1,4 @@
+import { Duration } from "moment";
 import moment from "moment/moment";
 import { useEffect, useState } from "react";
 import isDevMode from "util/isDevMode";
@@ -11,31 +12,53 @@ export const isAfterSpecificTimestamp = (timestampWithTimezone: moment.MomentInp
 export type TimedFeatureToggleParams = (
     | { enabledBefore: moment.MomentInput; enabledAfter?: undefined }
     | { enabledBefore?: undefined; enabledAfter: moment.MomentInput }
-) & { enabledInDevMode?: boolean };
+    ) & { enabledInDevMode?: boolean };
 
 export const useTimedFeatureToggle = ({
-    enabledBefore,
-    enabledAfter,
-    enabledInDevMode = false,
-}: TimedFeatureToggleParams) => {
+                                          enabledBefore,
+                                          enabledAfter,
+                                          enabledInDevMode = false,
+                                      }: TimedFeatureToggleParams) => {
     const [isEnabled, setEnabled] = useState(false);
+    const [timeRemaining, setTimeRemaining] = useState<Duration | undefined>(undefined);
 
     useEffect(() => {
-        const intervalHandle = setInterval(
-            () =>
-                setEnabled(
-                    enabledBefore
-                        ? !isAfterSpecificTimestamp(enabledBefore!)
-                        : isAfterSpecificTimestamp(enabledAfter!),
+        const intervalHandle = setInterval(() => {
+            const enabled = enabledBefore
+                ? !isAfterSpecificTimestamp(enabledBefore!)
+                : isAfterSpecificTimestamp(enabledAfter!);
+
+            setEnabled(enabled);
+
+            if ((enabledAfter && enabled) || (enabledBefore && !enabled)) {
+                setTimeRemaining(undefined);
+                clearInterval(intervalHandle);
+                return;
+            }
+
+            setTimeRemaining(
+                moment.duration(
+                    enabledAfter
+                        ? moment.utc().diff(moment.utc(enabledAfter))
+                        : moment.utc(enabledBefore).diff(moment.utc()),
                 ),
-            1000,
-        );
-        return () => clearInterval(intervalHandle);
+            );
+        }, 1000);
+        return () => {
+            try {
+                clearInterval(intervalHandle);
+            } catch (_) {
+                // ok
+            }
+        };
     }, [enabledBefore, enabledAfter]);
 
     if (enabledInDevMode && isDevMode) {
-        return true;
+        return { isEnabled: true, timeRemaining: undefined };
     }
 
-    return isEnabled;
+    return {
+        isEnabled,
+        timeRemaining,
+    };
 };
