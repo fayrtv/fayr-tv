90c8b7e61fcd6e44e10509ada114e624cc2f8f56
FAYR Engineering <73653296+fayrtv@users.noreply.github.com>
2022-09-26 09:30:58 +0200

    added style components due to mockup

--- packages/fayr-business-card/components/InfoBlock.tsx
+++ packages/fayr-business-card/components/InfoBlock.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 function Block() {
   return (
-    <div className="mt-10 flex items-center justify-around bg-green-700">
+    <div className="fixed left-[] items-center justify-around text-base font-sfproregular bg-[#141A24]">
           <a
             href="https://vcard.link/card/DC3r.vcf"
             className="mt-4 w-[100%] p-6 text-left"
           >
-            <h3 className="text-base font-sfproregular">
+            <h3 className="text-lg font-sfprobold">
               Tin Votan
             </h3>
-
-            <p className="mt-4 text-base font-sfproregular">
-              Test 123
+            <p className="mt-1">
+              CEO & Founder
+            </p>
+            <p className="mt-1">
+              FAYR GmbH
             </p>
             
           </a>
--- packages/fayr-business-card/pages/index.tsx
+++ packages/fayr-business-card/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
 
         <InfoBlock/>
 
-        <ContactsBlock/>
+        {/* <ContactsBlock/> */}
         
       </main>
     </div>
