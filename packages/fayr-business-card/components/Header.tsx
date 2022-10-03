e7edb88940cf5809e77f76f980c105f55202cec7
FAYR Engineering <73653296+fayrtv@users.noreply.github.com>
2022-10-03 22:47:48 +0200

    update business card

--- packages/fayr-business-card/components/Header.tsx
+++ packages/fayr-business-card/components/Header.tsx
@@ -5,8 +5,8 @@ function Header() {
     <div className='fixed top-20'>
         <img 
           src="https://fayr-logo-v001.s3.eu-central-1.amazonaws.com/svg/fayr_logo_main.svg" 
-          width={50}
-          height={50}
+          width={65}
+          height={65}
         />
     </div>
   )
--- packages/fayr-business-card/components/InfoBlock.tsx
+++ packages/fayr-business-card/components/InfoBlock.tsx
@@ -2,18 +2,27 @@ import React from 'react'
 
 function Block() {
   return (
-    <div className="fixed left-[] items-center justify-around text-base font-sfproregular bg-[#141A24]">
+    <div className="fixed w-10/12 pt-14 items-center justify-around text-base font-sfproregular bg-[#141A24]">
+
+      <img
+        src="https://fayr-image-library.s3.eu-central-1.amazonaws.com/team-pictures/profil_pic.png"
+        width="30%"
+        className="inline-flex items-center justify-center rounded-full p-3"
+      />
           <a
             href="https://vcard.link/card/DC3r.vcf"
-            className="mt-4 w-[100%] p-6 text-left"
+            className="px-6 pb-8 text-left"
           >
-            <h3 className="text-lg font-sfprobold">
+            
+            <h3 className="text-lg mx-8 font-sfprobold">
               Tin Votan
             </h3>
-            <p className="mt-1">
+
+            <p className="mt-1 mx-8">
               CEO & Founder
             </p>
-            <p className="mt-1">
+
+            <p className="mt-1 mx-8">
               FAYR GmbH
             </p>
             
